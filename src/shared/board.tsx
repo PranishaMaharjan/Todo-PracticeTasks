e5dbd40c2df23eeeda805a7061698a9b8c02b9b3
Pranisha <pranishaworks@gmail.com>
2025-08-26 14:02:01 +0545

    dnd

---
+++ src/shared/board.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React, { useState } from "react";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop";
+
+
+const Board = () => {
+    return (
+    const [data, setData]
+)
+}
+
+export default Board;
